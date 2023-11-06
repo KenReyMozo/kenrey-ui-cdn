8f61e9a242ca7ce4425d3d8fd2b3ef1172002eaa
KendallRey <107897863+KendallRey@users.noreply.github.com>
2023-11-06 11:49:26 +0800

    added list of shades for iterating components sample

---
+++ src/apps/List.ts
@@ -0,0 +1,18 @@
+import { Shade } from "../component/component"
+
+type Shades = {
+    id : string
+    value : Shade
+}[]
+
+export const Shades : Shades = [
+  { id: 'shade-100', value: '100' },
+  { id: 'shade-200', value: '200' },
+  { id: 'shade-300', value: '300' },
+  { id: 'shade-400', value: '400' },
+  { id: 'shade-500', value: '500' },
+  { id: 'shade-600', value: '600' },
+  { id: 'shade-700', value: '700' },
+  { id: 'shade-800', value: '800' },
+  { id: 'shade-900', value: '900' }
+]
